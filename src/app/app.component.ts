f6f97e09833e82da213a3a15e5ded2012c3149a0
Weber Alvarenga <weber.alvarenga@gmail.com>
2022-06-13 20:49:08 -0300

    Alteração para React Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,50 @@
 import { ModalService } from './shared/components/modal/services/modal.service';
-import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { Component, ViewChild, TemplateRef, OnInit } from '@angular/core';
 import { ModalRef } from './shared/components/modal/models/modal-ref';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   @ViewChild('userData') public modalTemplateRef: TemplateRef<any>;
 
   title = 'a11y-p2';
 
   public firstName = 'Weber';
   public modalRef: ModalRef;
+  public form: FormGroup; // ReactiveForm
 
 
-  constructor(private modalService: ModalService){}
+  constructor(
+    private modalService: ModalService,
+    private formBuilder: FormBuilder /*ReactiveForm*/){}
+
+  // ReactiveForm
+  public ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: ['Weber', Validators.required],
+      surName: ['', Validators.required],
+      age: ['', Validators.required],
+      subscribe: [false]
+    });
+
+  }
 
   public show(): void {
     this.modalRef = this.modalService.open({
                       templateRef: this.modalTemplateRef,
                       title: 'Detalhes do Usuário'
                     });
+  }
+
+  public submit(): void {
+
+    if (!this.form.invalid) {
+      console.log(this.form.value);
+      this.modalRef.close();
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     ModalModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
